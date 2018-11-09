b53d9fd46ef742e4da49ebf2b37391e931c1e24d
sascha <sastrand@pdx.edu>
2018-11-09 06:51:47 -0800

    changed function name

--- my_hash.py
+++ my_hash.py
@@ -11,7 +11,7 @@ def my_hash_2(s):
     int_vals = [c**i for i, c in enumerate(int_vals)]
     return sum(int_vals)
 
-def time_hash(hash_fun, uuids):
+def run_hash(hash_fun, uuids):
     hs = [hash_fun(uuid) for uuid in uuids]
     collision_rate = (len(hs) - len(set(hs))) / len(hs)
     collision_rate = round(collision_rate * 100, 2)
@@ -27,13 +27,13 @@ if __name__ == '__main__':
         uuid_full = f.readlines()
 
     print("Hash 0 collision rate: {}"
-           .format(time_hash(my_hash_0, uuid_full)))
+           .format(run_hash(my_hash_0, uuid_full)))
 
     print("Hash 1 collision rate: {}"
-           .format(time_hash(my_hash_1, uuid_full)))
+           .format(run_hash(my_hash_1, uuid_full)))
 
     print("Hash 2 collision rate: {}"
-       .format(time_hash(my_hash_2, uuid_full)))
+       .format(run_hash(my_hash_2, uuid_full)))
 
     print("Python hash collision rate: {}"
-        .format(time_hash(hash, uuid_full)))
+        .format(run_hash(hash, uuid_full)))
